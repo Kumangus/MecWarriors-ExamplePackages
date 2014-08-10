90d7bc2e92721c85376a8343c4bd930e562eaf1b
aormsby <aormsby@.(none)>
2014-08-10 00:40:56 -0500

    ScriptingRootMotion tutorial project added, new folder for commonly used tutorial assets

---
+++ ScriptingRootMotion/Assets/CharController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+	Animator animController;
+
+	enum MovementState
+	{
+		Idle = 0,
+		Walking = 1,
+		Running = 2
+	}
+	MovementState currentMovementState;
+
+	void Start ()
+	{
+		animController = GetComponent<Animator>();
+	}
+
+	void Update ()
+	{
+		float verticalAxis = Input.GetAxis("Vertical");
+
+		if (verticalAxis != 0)
+		{
+			if (Input.GetKey(KeyCode.LeftShift))
+				currentMovementState = MovementState.Running;
+			else
+				currentMovementState = MovementState.Walking;
+		}
+		else
+			currentMovementState = MovementState.Idle;
+
+		animController.SetInteger("MovementState", (int)currentMovementState);
+		animController.SetFloat("VerticalAxis", verticalAxis);
+	}
+
+//	void OnAnimatorMove()
+//	{
+//		Vector3 velocity = animController.deltaPosition / Time.deltaTime;
+//		velocity.y = rigidbody.velocity.y;
+//
+//		rigidbody.velocity = velocity;
+//	}
+}
