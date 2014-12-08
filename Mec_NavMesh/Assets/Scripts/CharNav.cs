160f7dfb312605bf118ef9f66eff409b28af9934
aormsby <aormsby@.(none)>
2014-12-07 18:29:57 -0600

    setting up NavMesh project

---
+++ Mec_NavMesh/Assets/Scripts/CharNav.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharNav : MonoBehaviour {
+
+	public Animator animController;
+
+	public enum MoveState {Idle, Walking}
+	public MoveState moveState;
+
+	public NavMeshAgent navMeshAgent;
+
+	public bool moveOnStart;
+	public Vector3 startDestination;
+
+	void Start ()
+	{
+		if (moveOnStart)
+			navMeshAgent.destination = startDestination;
+	}
+
+	void Update ()
+	{
+		if (navMeshAgent.hasPath)
+			moveState = MoveState.Walking;
+		else
+			moveState = MoveState.Idle;
+
+		animController.SetInteger("MoveState", (int)moveState);
+	}
+
+	void OnAnimatorMove ()
+	{
+		if (moveState == MoveState.Walking)
+		{
+			navMeshAgent.velocity = animController.deltaPosition / Time.deltaTime;
+			transform.rotation = Quaternion.LookRotation(navMeshAgent.desiredVelocity);
+		}
+	}
+}
---
+++ Mec_NavMesh/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	public CharNav charNav;
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHit;
+
+			if (Physics.Raycast(ray, out rayHit, 100f))
+			{
+				charNav.navMeshAgent.destination = rayHit.point;
+			}
+		}
+	}
+}
