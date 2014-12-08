338fcdc6281e11cac175df8eefc859e95ad98aa7
aormsby <aormsby@.(none)>
2014-12-07 18:54:05 -0600

    added Speed and Angular Speed connections, commented scripts

--- Mec_NavMesh/Assets/Scripts/ClickToMove.cs
+++ Mec_NavMesh/Assets/Scripts/ClickToMove.cs
@@ -3,19 +3,18 @@ using System.Collections;
 
 public class ClickToMove : MonoBehaviour {
 
-	public CharNav charNav;
+	public CharNav charNav;	//link to CharNav script on UnityGuy1
 
 	void Update ()
 	{
+		//set character destination to point of mouse click
 		if (Input.GetMouseButtonDown(0))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit rayHit;
 
 			if (Physics.Raycast(ray, out rayHit, 100f))
-			{
-				charNav.navMeshAgent.destination = rayHit.point;
-			}
+				charNav.navAgent.destination = rayHit.point;
 		}
 	}
 }
